e1b1c635cab04fe8fdb0d6a0f8e22e3ed8c9d595
Alex Li <sf.alexli@gmail.com>
2020-07-09 16:22:57 -0700

    style navbar-menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,17 +19,18 @@
         <span aria-hidden="true"></span>
       </a>
     </div>
+
     <!-- Menu -->
     <div v-bind:class="{'navbar-menu':true, 'is-active':isActive}">
       <div class="navbar-start">
         <a href="#" class="navbar-item">Features</a>
         <a href="#" class="navbar-item">Pricing</a>
         <a href="#" class="navbar-item">Resources</a>
+        <div class="navbar-divider"></div>
       </div>
-
       <div class="navbar-end">
         <a href="" class="navbar-item">Login</a>
-        <a href="" class="navbar-item">Sign up</a>
+        <a href="" class="navbar-item navbar-signup">Sign up</a>
       </div>
     </div>
   </nav>
@@ -56,8 +57,48 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '../styles/style-guide.scss';
+$item-percentage: 80%;
+
 .navbar {
   padding: 1em 1em;
   //border: 1px solid red;
+
+  &-item {
+    @media screen and (max-width: 1023px) {
+      color: white;
+      font-weight: 600;
+      margin: 1em 0;
+    }
+  }
+
+  &-menu {
+    @media screen and (max-width: 1023px) {
+      background-color: $dark-violet;
+      border-radius: 10px;
+      width: 95%;
+      margin: auto;
+    }
+  }
+
+  &-divider {
+    @media screen and (max-width: 1023px) {
+      height: 1px;
+      background-color: hsla(0, 0%, 66%, 0.225);
+      display: block;
+      width: $item-percentage;
+      margin: 1em auto;
+    }
+  }
+
+  &-signup {
+    @media screen and (max-width: 1023px) {
+      background-color: $cyan;
+      width: $item-percentage;
+      margin: auto;
+      border-radius: 2em;
+      margin-bottom: 2em;
+    }
+  }
 }
 </style>
